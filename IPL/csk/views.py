37c423ea1f236f9b39fb1c5509c839eb88afd8f7
Akash26-coder <aa.nayak01@gmail.com>
2024-11-27 23:35:15 +0530

    Add files via upload

---
+++ IPL/csk/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def csk(request):
+    return HttpResponse('THIS IS THE BEST TEAM')
---
+++ IPL/rcb/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def rcb(request):
+    return HttpResponse('This is the worst team of ipl')
